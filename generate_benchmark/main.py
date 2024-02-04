9f5af428438f6daaa6e5dac68e6fd3839a1de239
Doaa_Hassan <hsdoaa@gmail.com>
2024-02-04 09:32:35 -0500

    Update main.py

--- generate_benchmark/main.py
+++ generate_benchmark/main.py
@@ -21,15 +21,15 @@ sys.argv[2] = args.ref_input
 sys.argv[4] = args.fastq_input
 ##################################################################################
 
-with open('hela.sam','w') as f:
+with open('hek.sam','w') as f:
     subprocess.run(["minimap2", "-ax", "map-ont", "--split-prefix", "/tmp/temp_name", sys.argv[2], sys.argv[4]], stdout=f) 
 subprocess.run(["nanopolish" ,"index", "-d", "fast5_files/", sys.argv[4]])
-with open('hela.bam','w') as f1:
-    subprocess.run(["samtools" ,"view", "-S", "-b", "hela.sam"],stdout=f1)
-subprocess.run(["samtools" ,"sort", "hela.bam", "-o", "hela.sorted.bam"])
-subprocess.run(["samtools" ,"index", "hela.sorted.bam"])
-subprocess.run(["samtools","quickcheck" ,"hela.sorted.bam"])
-with open('hela-reads-ref.eventalign.txt','w') as f3:
-    subprocess.run(["nanopolish","eventalign" ,"--reads", sys.argv[4], "--bam", "hela.sorted.bam","--genome", sys.argv[2], "--scale-events"], stdout=f3)
+with open('hek.bam','w') as f1:
+    subprocess.run(["samtools" ,"view", "-S", "-b", "hek.sam"],stdout=f1)
+subprocess.run(["samtools" ,"sort", "hek.bam", "-o", "hek.sorted.bam"])
+subprocess.run(["samtools" ,"index", "hek.sorted.bam"])
+subprocess.run(["samtools","quickcheck" ,"hek.sorted.bam"])
+with open('hek-reads-ref.eventalign.txt','w') as f3:
+    subprocess.run(["nanopolish","eventalign" ,"--reads", sys.argv[4], "--bam", "hek.sorted.bam","--genome", sys.argv[2], "--scale-events"], stdout=f3)
 subprocess.run(["python","gen_coors_Nm.py"])
 subprocess.run(["python","extract_nm.py",])
